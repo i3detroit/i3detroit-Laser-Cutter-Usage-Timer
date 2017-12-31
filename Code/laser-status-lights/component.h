bf6c8b34e23414566a9989a5e90494c59907f8cd
mkfink <mkfink@gmail.com>
2017-12-31 14:39:01 -0500

    cleanup

--- Code/laser-status-lights/component.h
+++
@@ -1,8 +0,0 @@
-struct component
-{
-  int Pin;      //input pin number
-  int OffLED;   //LED # indicating component is off
-  int OnLED;    //LED # indicating component is on
-  bool State;    //State of the component input pin
-  char* mqttTopic;
-};
